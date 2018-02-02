8b854d4aa4483f88577cf965cb901c620dbf13cf
Hayden Jamieson <haydenjamieson8@gmail.com>
2018-02-02 14:53:32 +0000

    FIRBase messaging onconfigured, see description
    Cannot configure with Firebase due to no apple developer program

--- For Sale/AppDelegate.swift
+++ For Sale/AppDelegate.swift
@@ -23,6 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         print("Did register for Notifications")
+        print(Messaging.messaging().fcmToken)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
